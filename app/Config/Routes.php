5351bc483f325222334a7cc0d04ed64e98bf174d
drarvindc <drarvindc@users.noreply.github.com>
2025-08-10 06:40:33 +0530

    2

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,11 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->group('admin/tools', ['filter' => 'adminauth'], static function($routes){
+    $routes->get('migrate', 'Admin\\MigrateController::index');
+    $routes->post('migrate/run', 'Admin\\MigrateController::run');
+    $routes->post('migrate/rollback', 'Admin\\MigrateController::rollback');
+    $routes->post('migrate/seed-species', 'Admin\\MigrateController::seedSpecies');
+    $routes->get('db-status', 'Admin\\DbStatusController::index');
+});
+
