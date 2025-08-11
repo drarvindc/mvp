304cbea7a8fa3ae519985eeee51f121d228e1f05
drarvindc <drarvindc@users.noreply.github.com>
2025-08-12 00:03:56 +0530

    up

--- app/Config/Cookie.php
+++ app/Config/Cookie.php
@@ -3,105 +3,31 @@
 namespace Config;
 
 use CodeIgniter\Config\BaseConfig;
-use DateTimeInterface;
 
 class Cookie extends BaseConfig
 {
-    /**
-     * --------------------------------------------------------------------------
-     * Cookie Prefix
-     * --------------------------------------------------------------------------
-     *
-     * Set a cookie name prefix if you need to avoid collisions.
-     */
+    /** Prefix to prepend to all cookie names. MUST be a string (empty string is fine). */
     public string $prefix = '';
 
-    /**
-     * --------------------------------------------------------------------------
-     * Cookie Expires Timestamp
-     * --------------------------------------------------------------------------
-     *
-     * Default expires timestamp for cookies. Setting this to `0` will mean the
-     * cookie will not have the `Expires` attribute and will behave as a session
-     * cookie.
-     *
-     * @var DateTimeInterface|int|string
-     */
-    public $expires = 0;
-
-    /**
-     * --------------------------------------------------------------------------
-     * Cookie Path
-     * --------------------------------------------------------------------------
-     *
-     * Typically will be a forward slash.
-     */
-    public string $path = '/';
-
-    /**
-     * --------------------------------------------------------------------------
-     * Cookie Domain
-     * --------------------------------------------------------------------------
-     *
-     * Set to `.your-domain.com` for site-wide cookies.
-     */
+    /** Cookie domain (keep empty to default to current host). */
     public string $domain = '';
 
-    /**
-     * --------------------------------------------------------------------------
-     * Cookie Secure
-     * --------------------------------------------------------------------------
-     *
-     * Cookie will only be set if a secure HTTPS connection exists.
-     */
-    public bool $secure = false;
+    /** Cookie path. */
+    public string $path = '/';
 
-    /**
-     * --------------------------------------------------------------------------
-     * Cookie HTTPOnly
-     * --------------------------------------------------------------------------
-     *
-     * Cookie will only be accessible via HTTP(S) (no JavaScript).
-     */
+    /** Secure (true because you’re on HTTPS). */
+    public bool $secure = true;
+
+    /** HttpOnly flag. */
     public bool $httponly = true;
 
-    /**
-     * --------------------------------------------------------------------------
-     * Cookie SameSite
-     * --------------------------------------------------------------------------
-     *
-     * Configure cookie SameSite setting. Allowed values are:
-     * - None
-     * - Lax
-     * - Strict
-     * - ''
-     *
-     * Alternatively, you can use the constant names:
-     * - `Cookie::SAMESITE_NONE`
-     * - `Cookie::SAMESITE_LAX`
-     * - `Cookie::SAMESITE_STRICT`
-     *
-     * Defaults to `Lax` for compatibility with modern browsers. Setting `''`
-     * (empty string) means default SameSite attribute set by browsers (`Lax`)
-     * will be set on cookies. If set to `None`, `$secure` must also be set.
-     *
-     * @var ''|'Lax'|'None'|'Strict'
-     */
-    public string $samesite = 'Lax';
+    /** SameSite policy: 'Lax' is good for auth. */
+    public string $sameSite = 'Lax';
 
     /**
-     * --------------------------------------------------------------------------
-     * Cookie Raw
-     * --------------------------------------------------------------------------
-     *
-     * This flag allows setting a "raw" cookie, i.e., its name and value are
-     * not URL encoded using `rawurlencode()`.
-     *
-     * If this is set to `true`, cookie names should be compliant of RFC 2616's
-     * list of allowed characters.
-     *
-     * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie#attributes
-     * @see https://tools.ietf.org/html/rfc2616#section-2.2
+     * Default lifetime (in seconds) for “remember me” cookies.
+     * 0 means “session cookie” (expires when browser closes).
+     * Your login code should set an explicit expiry when Remember Me is checked.
      */
-    public bool $raw = false;
+    public int $expires = 0;
 }
